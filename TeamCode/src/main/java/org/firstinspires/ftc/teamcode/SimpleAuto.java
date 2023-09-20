0f54f3930f4db3fb89163b20d55a4d495185f7a3
jander15 <45211598+jander15@users.noreply.github.com>
2023-09-20 17:08:41 -0600

    set up robot and chassis class partially

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAuto.java
@@ -1,17 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-public class SimpleTeleOp extends LinearOpMode {
+@Autonomous(name="Simple OpMode", group="Linear OpMode")
+//@Disabled
+public class SimpleAuto extends LinearOpMode {
 
     public Robot robot = new Robot();
     private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode() throws InterruptedException {
-
-
 
         waitForStart();
         runtime.reset();
@@ -20,7 +20,6 @@ public class SimpleTeleOp extends LinearOpMode {
         while (opModeIsActive()) {
             robot.driveStraight(10,.8);
         }
-
 
     }
 

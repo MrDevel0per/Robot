8bd9c2e3a67b3aa9af73fd540479104fab1a998e
Owen Cruz-Abrams <mrdeveloper081@gmail.com>
2023-09-20 16:58:38 -0600

    Add our loop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleTeleOp.java
@@ -1,15 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class SimpleTeleOp extends LinearOpMode {
 
     public Robot robot = new Robot();
+    private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        robot.driveStraight(10,.8);
+
+
+        waitForStart();
+        runtime.reset();
+
+        // Call our loop
+        while (opModeIsActive()) {
+            robot.driveStraight(10,.8);
+        }
+
 
     }
+
 }

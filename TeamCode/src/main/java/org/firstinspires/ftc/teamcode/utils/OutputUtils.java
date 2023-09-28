bfcd28e352aca4c1b3a789511c5b38e4a4ced52e
Owen Cruz-Abrams <mrdeveloper081@gmail.com>
2023-09-28 17:55:14 -0600

    Create Utils in order to print out data

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/OutputUtils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/OutputUtils.java
@@ -5,14 +5,23 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.tel
 public class OutputUtils {
 
     /**
-     * `print` outputs the given `caption` and `value` to the robot's telemetry.
+     * `print` outputs the given `caption` and `value` to the robot's telemetry. We also implimented another verison of
+     * `soloPrint`, which has no caption.
+     *
      * @param caption The caption to give the output in the console.
-     * @param value The actual value to be outputted.
+     * @param value   The actual value to be outputted.
      */
     public static void print(String caption, Object value) {
         telemetry.addData(caption, value);
         telemetry.update();
     }
+
+    public static void soloPrint(Object value) {
+        telemetry.addData("", value);
+        telemetry.update();
+    }
+
+
 
 
 }

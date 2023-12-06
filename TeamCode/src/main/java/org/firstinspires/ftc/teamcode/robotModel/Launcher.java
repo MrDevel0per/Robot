0a2dde17625b013e691c263b62b364e5f7aaa5ce
Owen Cruz-Abrams <mrdeveloper081@gmail.com>
2023-12-06 08:11:28 -0700

    Begin work on launcher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotModel/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotModel/Launcher.java
@@ -1,4 +1,21 @@
 package org.firstinspires.ftc.teamcode.robotModel;
 
 public class Launcher {
+
+    // Return the required velocity of the launcher from the variables
+    public double calculateVelocity(double distance, double airPressure) {
+        // MARK: Constants
+        // TODO: Setup
+        double WING_AREA = 1;
+        // TODO: Oliver: Calculate
+        double COEFFICIENT_OF_LIFT = 1;
+        return 0.0;
+    }
+
+            // Return the required motor speed (0-1) from the velocity
+            public double calculateMotorSpeed(double velocity) {
+        // Multiplier of motor speed outputted
+        double GEAR_RAIO = 5;
+                    return 0.0;
+            }
 }

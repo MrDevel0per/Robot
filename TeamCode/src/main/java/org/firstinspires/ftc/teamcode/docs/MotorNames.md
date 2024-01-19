523557b2567eda01826a9e30ab1b6144a24c6510
Owen Cruz-Abrams <mrdeveloper081@gmail.com>
2024-01-18 19:37:37 -0700

    Comment out servos and do motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/docs/MotorNames.md
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/docs/MotorNames.md
@@ -1,9 +1,9 @@
 # Motor Names
 ## Wheel Motors
-- Left Front: `left_front`
-- Right Front: `right_front`
-- Left Rear: `left_rear`
-- Right Rear: `right_rear`
+- Left Front: `left_front`: Port `2` on the Expansion Hub
+- Right Front: `right_front`: Port `3` on the Control Hub
+- Left Rear: `left_rear`: Port `0` on the Expansion Hub
+- Right Rear: `right_rear`: Port `1` on the Expansion Hub
 ## Arm Motors
 See this code:
 ```java
@@ -14,10 +14,11 @@ this.leftRotator = hardwareMap.get(DcMotorEx.class, "left_rotator");
         this.rightFrontShovel = hardwareMap.get(Servo.class, "right_front_shovel");
         this.leftRearShovel = hardwareMap.get(Servo.class, "left_rear_shovel");
         this.rightRearShovel = hardwareMap.get(Servo.class, "right_rear_shovel");
-``
-- Left Rotator: `left_rotator`
-- Right Rotator: `right_rotator`
-- Up Down Motor: `up_down_motor`
+```
+- Left Rotator: `left_rotator`: Port `2` on the Control Hub
+- Right Rotator: `right_rotator`: Port `1` on the Control Hub
+- Up Down Motor: `up_down_motor`: Port `3` on the Expansion Hub
+## Shovel Servos
 - Left Front Shovel Servo: `left_front_shovel`
 - Right Front Shovel Servo: `right_front_shovel`
 - Left Rear Shovel Servo: `left_rear_shovel`
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotModel/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotModel/Arm.java
@@ -14,10 +14,10 @@ public class Arm {
     private DcMotorEx upDownMotor;
 
     // Motor for controlling forward/backward of shovel
-    private CRServo leftFrontShovel;
-    private CRServo rightFrontShovel;
-    private CRServo leftRearShovel;
-    private CRServo rightRearShovel;
+//    private CRServo leftFrontShovel;
+//    private CRServo rightFrontShovel;
+//    private CRServo leftRearShovel;
+//    private CRServo rightRearShovel;
 
     private HardwareMap hardwareMap;
 
@@ -26,10 +26,10 @@ public class Arm {
         this.leftRotator = hardwareMap.get(DcMotorEx.class, "left_rotator");
         this.rightRotator = hardwareMap.get(DcMotorEx.class, "right_rotator");
         this.upDownMotor = hardwareMap.get(DcMotorEx.class, "up_down_motor");
-        this.leftFrontShovel = hardwareMap.get(CRServo.class, "left_front_shovel");
-        this.rightFrontShovel = hardwareMap.get(CRServo.class, "right_front_shovel");
-        this.leftRearShovel = hardwareMap.get(CRServo.class, "left_rear_shovel");
-        this.rightRearShovel = hardwareMap.get(CRServo.class, "right_rear_shovel");
+//        this.leftFrontShovel = hardwareMap.get(CRServo.class, "left_front_shovel");
+//        this.rightFrontShovel = hardwareMap.get(CRServo.class, "right_front_shovel");
+//        this.leftRearShovel = hardwareMap.get(CRServo.class, "left_rear_shovel");
+//        this.rightRearShovel = hardwareMap.get(CRServo.class, "right_rear_shovel");
     }
 
     public void rotate(double power) {
@@ -46,10 +46,10 @@ public class Arm {
         // Convert it to 0 to 1 - -1 is now 0, 0 is now .5, and 1 is now 1
         power = (power + 1) / 2;
         // Continuously rotate servos
-        leftFrontShovel.setPower(power);
-        rightFrontShovel.setPower(power);
-        leftRearShovel.setPower(power);
-        rightRearShovel.setPower(power);
+//        leftFrontShovel.setPower(power);
+//        rightFrontShovel.setPower(power);
+//        leftRearShovel.setPower(power);
+//        rightRearShovel.setPower(power);
 
     }
 }

4c903d3fbd64da4d2ee98fbc56c688becebc5bd5
Owen Cruz-Abrams <mrdeveloper081@gmail.com>
2024-02-09 16:52:45 -0700

    Beautify code in `Arm.java`

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotModel/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotModel/Arm.java
@@ -9,29 +9,17 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Arm {
-    // Motors for controlling rotate forward/backward
-    //private DcMotor leftRotator;
-    //private DcMotor rightRotator;
-
-    // Motor for controlling up/down
-    //private DcMotor upDownMotor;
     private DcMotor leftRotator;
     private DcMotor rightRotator;
     private Servo clawServo = null;
     private Servo clawServoTwo = null;
-
     private CRServo clawRotator = null;
-
-    // Motor for controlling open/close of the claw
     private HardwareMap hardwareMap;
     private Telemetry telemetry;
-
     public boolean isHolding = true;
 
     public Arm(HardwareMap hardwareMap) {
         this.hardwareMap = hardwareMap;
-        //this.upDownMotor = hardwareMap.get(DcMotor.class, "up_down_motor");
-        //upDownMotor.setDirection(DcMotor.Direction.REVERSE);
         this.leftRotator = hardwareMap.get(DcMotor.class, "left_rotator");
         leftRotator.setDirection(DcMotor.Direction.FORWARD);
         this.rightRotator = hardwareMap.get(DcMotor.class, "right_rotator");
@@ -42,9 +30,7 @@ public class Arm {
         clawServo.setDirection(Servo.Direction.FORWARD);
         this.clawServoTwo = hardwareMap.get(Servo.class, "claw_servo_two");
         clawServoTwo.setDirection(Servo.Direction.REVERSE);
-
     }
-
 
     public void grip() {
         clawServo.setPosition(1.0);
@@ -61,8 +47,6 @@ public class Arm {
     }
 
     public enum Position {
-        // TODO: Update to correct degrees
-
         GROUND(1),
         FIRST_LINE(2),
         SECOND_LINE(3),
@@ -73,76 +57,48 @@ public class Arm {
         private Position(int degrees) {
             this.DEGREES_OF_360 = degrees;
         }
-        }
+    }
+
     public void rotateArm(Position position) {
         isHolding = false;
-        // TODO: First, set current position to the bottom position
-        // TODO: Then, calculate necessary to get to ground
-        // 1. Set current position:
         int currentPosition = getArmRotation();
-
-        // 2. Calculate necessary movement:
         int targetPosition = position.DEGREES_OF_360;
         int difference = targetPosition - currentPosition;
-
-        // 3. Handle direction and movement:
-        int direction = 1; // Assume clockwise initially
-        if (Math.abs(difference) > 180) { // Check for shorter counter-clockwise rotation
+        int direction = 1;
+        if (Math.abs(difference) > 180) {
             direction = -1;
             difference = (360 - currentPosition) + targetPosition;
-            System.out.println("Difference: " + difference);
         }
-
-        // 4. Move motors smoothly:
-        while (Math.abs(difference) > 60) {// Adjust the threshold as needed
-            int power = (int) Math.signum(difference) * (Math.min(Math.abs(difference), 1)); // Scale power smoothly
-            System.out.println("Direction: " + direction);
-            System.out.println("Power: " + power);
+        while (Math.abs(difference) > 60) {
+            int power = (int) Math.signum(difference) * (Math.min(Math.abs(difference), 1));
             rightRotator.setPower(power * direction * 0.3);
             leftRotator.setPower(power * direction * 0.3);
-            difference = getArmRotation() - targetPosition; // Update difference
+            difference = getArmRotation() - targetPosition;
         }
-
-        // 5. Final adjustment:
         rightRotator.setPower(0);
         leftRotator.setPower(0);
         isHolding = true;
-        hold(); // Ensure final position accuracy
-
+        hold();
     }
 
-
-
     private void rotateArmToDesiredPos(int desiredPosition) {
-        // Calculate movement
-        // Calculate movement direction
-        // It's ok if the motor is off by [MAX_CLICKS_OFF] clicks
         double TICKS_PER_360_DEGREES = 537.7;
-//        int allowedErrorDegrees = 4/360;
         int allowedError = 1;
         int currentMotorPosition = this.getArmRotation();
         int difference = desiredPosition - currentMotorPosition;
-        // TODO: Make more powerful if needed
         double power = 0.5;
         if (difference > allowedError) {
-            // Move up
             rightRotator.setPower(power);
             leftRotator.setPower(power);
         } else if (difference < -allowedError) {
-            // Move down
             rightRotator.setPower(-power);
             leftRotator.setPower(-power);
         } else {
-            // We are at the right position
             rightRotator.setPower(0);
             leftRotator.setPower(0);
         }
-
     }
 
-    /*
-    Hold the claw in place
-     */
     public void hold() {
         long CHECK_EVERY_MILLISECONDS = 100;
         int startingMotorPosition = getArmRotation();
@@ -150,20 +106,15 @@ public class Arm {
         while (isHolding) {
             int currentMotorPosition = getArmRotation();
             if (currentMotorPosition != startingMotorPosition) {
-                // Oh noes! The motor has moved!
-                // We need to move the motor back
                 rotateArmToDesiredPos(startingMotorPosition);
             }
-
             while (System.nanoTime() - currentTime < CHECK_EVERY_MILLISECONDS) {
                 if (!isHolding) {
                     return;
                 }
-                continue;
             }
             currentTime = System.nanoTime();
         }
-
     }
 
     public void clawRotate(double power) {
@@ -174,14 +125,4 @@ public class Arm {
         leftRotator.setPower(power);
         rightRotator.setPower(power);
     }
-
-    /*public void upDown(double power) {
-
-        upDownMotor.setPower(power);
-    }*/
-
-
 }
-
-
-
